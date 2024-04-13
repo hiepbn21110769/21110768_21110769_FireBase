511f8ae96c6aa4091bcd2243d5ce96f8f89be0a3
hnghao11 <21110768@student.hcmute.edu.vn>
2024-04-13 14:59:45 +0700

    test

---
+++ app/src/main/java/com/example/firebaseexample/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.example.firebaseexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

4c08b1d99bac7f1899d6968b214d343fa0539770
hiepbui32 <hiepbui2812@gmail.com>
2024-04-13 14:48:44 +0700

    abc

--- app/src/main/java/com/example/firebaseexample/ViewAdapter.java
+++ app/src/main/java/com/example/firebaseexample/ViewAdapter.java
@@ -1,20 +1,13 @@
     package com.example.firebaseexample;
 
-    import android.annotation.SuppressLint;
-    import android.app.Activity;
-    import android.content.Intent;
     import android.graphics.Bitmap;
     import android.graphics.BitmapFactory;
-    import android.graphics.Color;
-    import android.view.ContextMenu;
     import android.view.LayoutInflater;
-    import android.view.MenuInflater;
     import android.view.View;
     import android.view.ViewGroup;
     import android.widget.ImageView;
 
     import androidx.annotation.NonNull;
-    import androidx.core.content.ContextCompat;
     import androidx.recyclerview.widget.RecyclerView;
 //    import com.bumptech.glide.Glide;
 
@@ -22,16 +15,11 @@
     import java.io.IOException;
     import java.lang.ref.WeakReference;
     import java.net.URL;
-    import java.util.ArrayList;
-    import java.util.HashSet;
     import java.util.List;
-    import java.util.Set;
 
     public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.ImageViewHolder>{
 
         private List<String> imageURLs;
-        private List<String> selectedItems = new ArrayList<>();
-
 
         public ViewAdapter(List<String> imageURLs) {
             this.imageURLs = imageURLs;
@@ -45,30 +33,14 @@
         }
 
         @Override
-        public void onBindViewHolder(@NonNull ImageViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+    //        String imageURL = imageURLs.get(position);
+    //        Glide.with(holder.imageView.getContext())
+    //                .load(imageURL)
+    //                .into(holder.imageView);
             String imageURL = imageURLs.get(position);
             // Sử dụng ImageLoad để load hình ảnh từ imageURL và hiển thị vào imageView
             new ImageLoad(  holder.imageView, imageURL).execute();
-
-            // Sự kiện click vào ImageView
-            holder.imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Chuyển đến màn hình mới và hiển thị ảnh ở kích thước lớn
-                    Intent intent = new Intent(v.getContext(), FullScreenImageActivity.class);
-                    intent.putExtra("imageURL",imageURL);
-                    v.getContext().startActivity(intent);
-                }
-            });
-
-            holder.imageView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    toggleSelection(position);
-                    return true;
-                }
-            });
-
 
         }
 
@@ -85,18 +57,5 @@
                 imageView = itemView.findViewById(R.id.imageView);
             }
         }
-
-        public void toggleSelection(int position) {
-            String item = imageURLs.get(position);
-            if (selectedItems.contains(item)) {
-                selectedItems.remove(item);
-            } else {
-                selectedItems.add(item);
-            }
-            notifyDataSetChanged();
-        }
-
-
-
 
     }
